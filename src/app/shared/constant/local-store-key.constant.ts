f9d00169fecfdb34278c3426cc5b0ece0e6994b9
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-25 21:32:49 +0530

    Adding user type to storage key

--- src/app/shared/constant/local-store-key.constant.ts
+++ src/app/shared/constant/local-store-key.constant.ts
@@ -1,15 +1,12 @@
 export class LocalStoreKey {
-	public static readonly SKIP_INTRO: string = 'C2_SKIP_INTRO';
-	public static readonly LOGGED_IN_SESSION_ID: string = 'C2_LOGGED_IN_SESSION_ID';
-	public static readonly LOGGED_IN_USER_EMAIL: string = 'C2_LOGGED_IN_USER_EMAIL';
-	public static readonly LOGGED_IN_USER_PASSWORD: string = 'C2_LOGGED_IN_USER_PASSWORD';
-	public static readonly LOGGED_IN_USER_ID: string = 'C2_LOGGED_IN_USER_ID';
-	public static readonly LOGGED_IN_USER_FIRST_NAME: string = 'C2_LOGGED_IN_USER_FIRST_NAME';
-	public static readonly LOGGED_IN_USER_LAST_NAME: string = 'C2_LOGGED_IN_USER_LAST_NAME';
-	public static readonly LOGGED_IN_USER_SECURITY_ANSWER_1: string = 'C2_LOGGED_IN_USER_SECURITY_ANSWER_1';
-	public static readonly LOGGED_IN_USER_SECURITY_ANSWER_2: string = 'C2_LOGGED_IN_USER_SECURITY_ANSWER_2';
-	public static readonly SELECTED_PROJECT_ID: string = 'C2_SELECTED_PROJECT_ID';
-	public static readonly SELECTED_PROJECT_NAME: string = 'C2_SELECTED_PROJECT_NAME';
-	public static readonly FILTER: string = 'C2_FILTER';
+	public static readonly SKIP_INTRO: string = 'KN_SKIP_INTRO';
+	public static readonly LOGGED_IN_SESSION_ID: string = '_uTk';
+	public static readonly LOGGED_IN_USER_EMAIL: string = '_uEm';
+	public static readonly LOGGED_IN_USER_ID: string = '_uId';
+	public static readonly LOGGED_IN_USER_FIRST_NAME: string = '_uFn';
+	public static readonly LOGGED_IN_USER_LAST_NAME: string = '_uLs';
+	public static readonly LOGGED_IN_USER_TYPE: string = '_uType';
+	public static readonly FILTER: string = 'KN_FILTER';
 	public static readonly LANGUAGE: string = '_lang';
+	public static readonly LOGGED_IN_USER_SKILLS: string = '_skls';
 }
