49916d8496ad154af1147cb4697dba408183fd1e
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-26 23:24:04 +0530

    Add title type enum

---
+++ src/app/shared/enum/title-type.enum.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Ranjoy Sen
+ * @email ranjoy.sen@rockwellcollins.com
+ * @create date 2021-06-12 21:54:28
+ * @modify date 2021-06-12 21:54:28
+ * @desc Title type enum
+ */
+
+export enum TitleTypeEnum {
+	MAIN_MENU,
+	INFO,
+	QUESTION,
+	VALIDATION,
+	CATEGORY,
+	PAGE,
+	PAGE_NO_TECH_DATA,
+	MODAL,
+	SUB_CATEGORY,
+	PRO_CATEGORY,
+	PANEL,
+	SELECTABLE_PANEL,
+	SOLO_TITLE_FORMAT,
+	INFO_TITLE_NO_ACTION,
+	SOLO_ITERABLE_TITLE_FORMAT,
+	SINGLE_ROW
+}
