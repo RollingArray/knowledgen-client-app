f75d17a060b68adc45671d414aa1594954448f8c
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-26 23:23:48 +0530

    Adding element type model

---
+++ src/app/shared/enum/element-type.enum.ts
@@ -0,0 +1,22 @@
+/**
+ * @author Ranjoy Sen
+ * @email ranjoy.sen@rockwellcollins.com
+ * @create date 2021-12-12 01:00:58
+ * @modify date 2021-12-12 01:00:58
+ * @desc Element type enum
+ */
+
+export enum ElementTypeEnum{
+    LEVEL_1 = "level-1",
+    LEVEL_2 = "level-2",
+    LEVEL_3 = "level-3",
+    PARA = "para",
+    PARA_IMAGE = "para-image",
+    LIST_IMAGE = "list-image",
+    LIST = "ol",
+    IMAGE = "image",
+    TEXT = "text",
+    UNFORMATTED_TEXT = "unformatted-text",
+    BOLD_TEXT = "bold-text",
+    HYPERLINK = "hyperlink"
+}
