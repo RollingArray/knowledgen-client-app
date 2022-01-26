465126f6ebe5481d0f5f88fc11f699b32004dc8a
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-26 23:22:48 +0530

    Knowledge base urls

--- src/app/shared/constant/api-urls.constant.ts
+++ src/app/shared/constant/api-urls.constant.ts
@@ -7,7 +7,7 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2021-11-15 21:34:14 
- * Last modified  : 2022-01-25 21:03:14
+ * Last modified  : 2022-01-26 19:46:12
  */
 
 import { environment } from "../../../environments/environment";
@@ -16,7 +16,7 @@ export class ApiUrls {
 	public static readonly API_ENDPOINT: string = environment.apiEndpoint;
 	public static readonly API_VERSION: string = "v1";
 	public static readonly API_BASE_PATH: string = ApiUrls.API_ENDPOINT + ApiUrls.API_VERSION;
-
+	public static readonly RA: string = 'https://rollingarray.co.in/';
 	// urls
 	public static readonly SIGN_IN: string = ApiUrls.API_BASE_PATH + "/user/sign/in";
 	public static readonly SIGN_UP: string = ApiUrls.API_BASE_PATH + "/user/sign/up";
@@ -27,6 +27,7 @@ export class ApiUrls {
 	public static readonly USER_PROFILE_UPDATE: string = ApiUrls.API_BASE_PATH + "/user/edit";
 
 	public static readonly COURSE_MATERIAL: string = ApiUrls.API_BASE_PATH + "/course/material/all";
+	public static readonly RECOMMENDED_COURSE_MATERIAL: string = ApiUrls.API_BASE_PATH + "/course/material/recommended/all";
 	public static readonly COURSE_MATERIAL_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/add";
 	public static readonly COURSE_MATERIAL_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/edit";
 	public static readonly COURSE_MATERIAL_DELETE: string = ApiUrls.API_BASE_PATH + "/course/material/delete";
@@ -47,4 +48,7 @@ export class ApiUrls {
 	public static readonly STUDENT_AVAILABILITY_PLANNER_ADD: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/add";
 	public static readonly STUDENT_AVAILABILITY_PLANNER_EDIT: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/edit";
 	public static readonly STUDENT_AVAILABILITY_PLANNER_DELETE: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/delete";
+
+	public static readonly KNOWLEDGE_BASE_ARTICLE_COMPONENTS: string = ApiUrls.API_BASE_PATH + "/course/material/article/component/all";
+	public static readonly KNOWLEDGE_BASE_ARTICLE_COMPONENT_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/article/component/add";
 }
